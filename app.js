3e5bf61bd0911b420ffc516bf14c482b8f26baf0
BananaMan <suvorov.andrey755@gmail.com>
2023-10-20 00:26:06 +0300

    20.10

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: ['http://localhost:3001', 'https://api.diplom.films.nomoredomainsrocks.ru'],
+  origin: ['http://localhost:3001', 'http://api.diplom.films.nomoredomainsrocks.ru'],
   credentials: true,
 }));
 
