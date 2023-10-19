38a73e6f24f7a1921aaa67043463a94867e7a2fb
BananaMan <suvorov.andrey755@gmail.com>
2023-10-20 01:06:29 +0300

    доделать

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const limiter = require('./utils/limiter');
 
 const routes = require('./routes');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
 app.use(helmet());
 
 app.use(cors({
-  origin: ['http://localhost:3001', ''],
+  origin: ['http://localhost:3000', 'https://diplom.films.nomoredomainsrocks.ru'],
   credentials: true,
 }));
 
